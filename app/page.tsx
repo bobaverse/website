8f28cf14629900e84a820ecf6a898205b834deb9
Nicholas St. Germain <nick@cajun.pro>
2023-03-16 18:25:33 -0500

    clean up home page responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ const Home: FC = () => {
   return (
     <div className="flex flex-col flex-wrap pt-4 h-full items-center select-none">
       <Image src={Logo} alt="logo" width={256} priority />
-      <span className="text-4xl font-semibold mt-4">Welcome to the BobaVerse</span>
-      <div className="flex center space-x-4 mt-4">
+      <span className="text-4xl font-semibold mt-4 text-center">Welcome to the BobaVerse</span>
+      <div className="flex flex-col sm:flex-row center space-y-4 sm:space-y-0 sm:space-x-4 mt-4">
         <BigButton text="Arcade" href="/#" />
         <BigButton text="Profile" href="/#" />
         <BigButton text="Leaderboards" href="/#" />
