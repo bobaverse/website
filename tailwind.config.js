fa2bead5abc1fbfff56cba7d1e426d8b78dd1e9f
Nicholas St. Germain <nick@cajun.pro>
2023-03-16 18:15:04 -0500

    update postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./interfaces/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     backdropBlur: {
